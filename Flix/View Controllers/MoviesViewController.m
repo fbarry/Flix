448d4c5816ba14d7d8402d9ab9204550801ede62
Fiona Barry <fbarry@fb.com>
2020-06-24 18:40:50 -0400

    Slight UI adjustment and added refresh control

--- Flix/View Controllers/MoviesViewController.m
+++ Flix/View Controllers/MoviesViewController.m
@@ -14,6 +14,7 @@
 
 @property (nonatomic, strong) NSArray *movies;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property (nonatomic, strong) UIRefreshControl *refreshControl;
 
 @end
 
@@ -25,24 +26,32 @@
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
     
-    // Do any additional setup after loading the view.
+    [self fetchMovies];
+    
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
+    [self.tableView insertSubview:self.refreshControl atIndex:0];
+}
+
+- (void) fetchMovies {
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
     NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
-           if (error != nil) {
-               NSLog(@"%@", [error localizedDescription]);
-           }
-           else {
-               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+            if (error != nil) {
+                NSLog(@"%@", [error localizedDescription]);
+            }
+            else {
+                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
                
-               self.movies = dataDictionary[@"results"];
-               [self.tableView reloadData];
-               
-               // TODO: Get the array of movies
-               // TODO: Store the movies in a property to use elsewhere
-               // TODO: Reload your table view data
-           }
+                self.movies = dataDictionary[@"results"];
+                [self.tableView reloadData];
+                   
+                // TODO: Get the array of movies
+                // TODO: Store the movies in a property to use elsewhere
+                // TODO: Reload your table view data
+            }
+        [self.refreshControl endRefreshing];
        }];
     [task resume];
 }
@@ -62,9 +71,9 @@
     if ([movie[@"poster_path"] isKindOfClass:[NSString class]]) {
         NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
         NSString *posterURLString = movie[@"poster_path"];
-        NSString *fullPosterURLString = [baseURLString stringByAppendingString: posterURLString];
+        NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
         NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
-        [cell.posterView setImageWithURL: posterURL];
+        [cell.posterView setImageWithURL:posterURL];
     }
     
     return cell;

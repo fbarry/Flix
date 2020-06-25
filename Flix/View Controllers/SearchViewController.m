fda82f830991255ae64774fe5826c7b6192b8a69
Fiona Barry <fbarry@fb.com>
2020-06-25 19:38:29 -0400

    Add search feature

---
+++ Flix/View Controllers/SearchViewController.m
@@ -0,0 +1,127 @@
+//
+//  SearchViewController.m
+//  Flix
+//
+//  Created by Fiona Barry on 6/25/20.
+//  Copyright © 2020 fbarry. All rights reserved.
+//
+
+#import "SearchViewController.h"
+#import "MovieCollectionCell.h"
+#import "UIImageView+AFNetworking.h"
+
+@interface SearchViewController () <UISearchBarDelegate, UICollectionViewDelegate, UICollectionViewDataSource>
+
+@property (nonatomic, strong) NSArray *movies;
+@property (nonatomic, strong) NSArray *filteredMovies;
+@property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
+@property (weak, nonatomic) UIAlertController *alertController;
+@property (strong, nonatomic) IBOutlet UISearchBar *searchBar;
+
+@end
+
+@implementation SearchViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.collectionView.delegate = self;
+    self.collectionView.dataSource = self;
+    self.searchBar.delegate = self;
+    
+    [self fetchMovies];
+    
+    UICollectionViewFlowLayout *layout = (UICollectionViewFlowLayout *) self.collectionView.collectionViewLayout;
+    
+    layout.minimumInteritemSpacing = 5;
+    layout.minimumLineSpacing = 5;
+    
+    CGFloat postersPerRow = 3;
+    CGFloat itemWidth = (self.collectionView.frame.size.width - layout.minimumInteritemSpacing * (postersPerRow - 1)) / postersPerRow;
+    CGFloat itemHeight = itemWidth * 1.5;
+    layout.itemSize = CGSizeMake(itemWidth, itemHeight);
+}
+
+- (void) fetchMovies {
+    
+    NSLog(@"fetchMovies");
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+            if (error != nil) {
+                self.alertController = [UIAlertController
+                                        alertControllerWithTitle:@"Cannot Load Movies"
+                                        message:@"The Internet connection appears to be offline."
+                                        preferredStyle:UIAlertControllerStyleAlert];
+                UIAlertAction *tryAgain = [UIAlertAction
+                                           actionWithTitle:@"Try Again"
+                                           style:UIAlertActionStyleDefault
+                                           handler:^(UIAlertAction * action) { [self fetchMovies]; }];
+                [self.alertController addAction:tryAgain];
+                [self presentViewController:self.alertController animated:YES completion:nil];
+            }
+            else {
+                NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+               
+                self.movies = dataDictionary[@"results"];
+                self.filteredMovies = self.movies;
+                [self.collectionView reloadData];
+                
+                NSLog(@"done fetching");
+            }
+       }];
+    [task resume];
+}
+- (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    MovieCollectionCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"MovieCollectionCell" forIndexPath:indexPath];
+    NSDictionary *movie = self.filteredMovies[indexPath.item];
+    
+    cell.posterView.image = nil;
+    
+    if ([movie[@"poster_path"] isKindOfClass:[NSString class]]) {
+        // NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+        // NSString *posterURLString = movie[@"poster_path"];
+        // NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
+        // NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
+        
+        NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+        NSString *posterURLString = movie[@"poster_path"];
+        NSString *fullPosterURLString = [baseURLString stringByAppendingString:posterURLString];
+        NSURL *posterURL = [NSURL URLWithString:fullPosterURLString];
+        NSLog(fullPosterURLString);
+        [cell.posterView setImageWithURL:posterURL];
+    }
+    
+    return cell;
+}
+
+- (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.filteredMovies.count;
+}
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    
+    if (searchText.length != 0) {
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"title contains[c] %@",searchText];
+        self.filteredMovies = [self.movies filteredArrayUsingPredicate:predicate];
+    }
+    else {
+        self.filteredMovies = self.movies;
+    }
+    
+    [self.collectionView reloadData];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end

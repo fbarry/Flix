929c08a4bacc418b40badfa26c75f68d6f4bce9c
Fiona Barry <fbarry@fb.com>
2020-06-26 15:59:27 -0400

    Added activity indicator to search view

--- Flix/View Controllers/SearchViewController.m
+++ Flix/View Controllers/SearchViewController.m
@@ -19,6 +19,7 @@
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 @property (strong, nonatomic) IBOutlet UISearchBar *searchBar;
 @property (weak, nonatomic) IBOutlet UILabel *searchLabel;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 
 @end
 
@@ -45,6 +46,8 @@
 }
 
 - (void) fetchMovies {
+    [self.activityIndicator startAnimating];
+    
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
@@ -64,6 +67,7 @@
                 self.movies = dataDictionary[@"results"];
                 self.filteredMovies = self.movies;
                 [self.collectionView reloadData];
+                [self.activityIndicator stopAnimating];
             }
        }];
     [task resume];
